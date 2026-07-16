0612e2cf7789d0b12d56122bd2536f0212846e40
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-07-16 19:43:59 +0100

    bitboards errata

--- src/bitboards/bitboards.h
+++ src/bitboards/bitboards.h
@@ -6,7 +6,7 @@ extern std::uint64_t bishopMoves[64];
 extern std::uint64_t rookMoves[64];
 extern std::uint64_t queenMoves[64];
 extern std::uint64_t kingMoves[64];
-extern std::uint64_t WhitePawnMoves[64]; // [0] for white, [1] for black
-extern std::uint64_t BlackPawnMoves[64]; // [0] for white, [1] for black
-extern std::uint64_t WhitePawnAttacks[64]; // [0] for white, [1] for black
-extern std::uint64_t BlackPawnAttacks[64]; // [0] for white, [1] for black
+extern std::uint64_t WhitePawnMoves[64]; 
+extern std::uint64_t BlackPawnMoves[64]; 
+extern std::uint64_t WhitePawnAttacks[64]; 
+extern std::uint64_t BlackPawnAttacks[64]; 

dd17e6cf025f367c42a5b1c4c9976c469d38193d
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-07-16 19:22:12 +0100

    added init for knightmoves

--- readme.md
+++ readme.md
@@ -1,29 +1,3 @@
 OmegaFish v0
 
-STRUCTURE:
-
-Generate Legal Moves
-_bitboards
-_magicbitboards
-Board Evaluation
-_heuristics
-Search
-_abpruning
-_quinescence
-
-
-
-
-
-Tutorial:
-
-Bit Boards: 
-start at a1 and then end at h8 in a s shape
-move up: +8*k
-move side: +d
-diagonal: +8*k + d
-...
-
-masks:
-occupied - white or black
-
+OmegaFish is a chess bot
--- src/bitboards/bitboards.cpp
+++ src/bitboards/bitboards.cpp
@@ -5,5 +5,39 @@ std::uint64_t knightMoves[64];
 void initializeKnightMoves() {
     for (int i = 0; i < 64; i++) {
         knightMoves[i] = 0;
+        if (i % 8 > 1) {
+            if (i / 8 > 0) {
+                knightMoves[i] |= 1ULL << (i - 10);
+            }
+            if (i / 8 < 7) {
+                knightMoves[i] |= 1ULL << (i + 6);
+            }
+        }
+        if (i % 8 < 6) {
+            if (i / 8 > 0) {
+                knightMoves[i] |= 1ULL << (i - 6);
+            }
+            if (i / 8 < 7) {
+                knightMoves[i] |= 1ULL << (i + 10);
+            }
+
+        }
+        if (i % 8 > 0) {
+            if (i / 8 > 1) {
+                knightMoves[i] |= 1ULL << (i - 17);
+            }
+            if (i / 8 < 6) {
+                knightMoves[i] |= 1ULL << (i + 15);
+            }
+        }
+        if (i % 8 < 7) {
+            if (i / 8 > 1) {
+                knightMoves[i] |= 1ULL << (i - 15);
+            }
+            if (i / 8 < 6) {
+                knightMoves[i] |= 1ULL << (i + 17);
+            }
+        }
     }
 }
+

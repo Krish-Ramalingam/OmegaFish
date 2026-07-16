47e767b7cf93968850e80b425fd15f600e553361
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-07-16 19:39:33 +0100

    added some more bitboards

--- src/bitboards/bitboards.cpp
+++ src/bitboards/bitboards.cpp
@@ -41,3 +41,52 @@ void initializeKnightMoves() {
     }
 }
 
+void initializeBishopMoves() {
+    for (int i = 0; i < 64; i++) {
+        bishopMoves[i] = 0;
+        int pos_x = i % 8;
+        int pos_y = i / 8;
+        for (int j = 1; j < 8; j++) {
+            if (pos_x + j < 8 && pos_y + j < 8) {
+                bishopMoves[i] |= 1ULL << (i + j * 9);
+            }
+            if (pos_x - j >= 0 && pos_y + j < 8) {
+                bishopMoves[i] |= 1ULL << (i + j * 7);
+            }
+            if (pos_x + j < 8 && pos_y - j >= 0) {
+                bishopMoves[i] |= 1ULL << (i - j * 7);
+            }
+            if (pos_x - j >= 0 && pos_y - j >= 0) {
+                bishopMoves[i] |= 1ULL << (i - j * 9);
+            }
+        }
+}
+
+void initializeRookMoves() {
+    for (int i = 0; i < 64; i++) {
+        rookMoves[i] = 0;
+        int pos_x = i % 8;
+        int pos_y = i / 8;
+        for (int j = 1; j < 8; j++) {
+            if (pos_x + j < 8) {
+                rookMoves[i] |= 1ULL << (i + j);
+            }
+            if (pos_x - j >= 0) {
+                rookMoves[i] |= 1ULL << (i - j);
+            }
+            if (pos_y + j < 8) {
+                rookMoves[i] |= 1ULL << (i + j * 8);
+            }
+            if (pos_y - j >= 0) {
+                rookMoves[i] |= 1ULL << (i - j * 8);
+            }
+        }
+    }
+}
+
+void initializeQueenMoves() {
+    for (int i = 0; i < 64; i++) {
+        queenMoves[i] = bishopMoves[i] | rookMoves[i];
+    }
+}
+
--- src/bitboards/bitboards.h
+++ src/bitboards/bitboards.h
@@ -2,3 +2,11 @@
 #include <cstdint>
 
 extern std::uint64_t knightMoves[64];
+extern std::uint64_t bishopMoves[64];
+extern std::uint64_t rookMoves[64];
+extern std::uint64_t queenMoves[64];
+extern std::uint64_t kingMoves[64];
+extern std::uint64_t WhitePawnMoves[64]; // [0] for white, [1] for black
+extern std::uint64_t BlackPawnMoves[64]; // [0] for white, [1] for black
+extern std::uint64_t WhitePawnAttacks[64]; // [0] for white, [1] for black
+extern std::uint64_t BlackPawnAttacks[64]; // [0] for white, [1] for black

0738c839c526fee8045945b45773581ceb95ef66
Krish Ramalingam <krishramalingam2008@gmail.com>
2026-07-17 21:31:25 +0100

    +

--- sandbox/example.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-#include <cstdint>
-
-int main() {
-    std::uint64_t whiteKings = 0x000000000000000FF; // Example bitboard for white kings
-    std::cout << std::__popcount(whiteKings) << std::endl; // Output the value of whiteKings
-    return 0;
-}
